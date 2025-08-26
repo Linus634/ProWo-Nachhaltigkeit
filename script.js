e05b5ca377fd031f93859499bccf23eecc651442
Linus634 <linusohse@icloud.com>
2025-08-26 09:29:52 +0000

    .

--- script.js
+++ script.js
@@ -9,26 +9,21 @@ import {
   Balance,
 } from "./library.js";
 
-const balance = new Balance(2000);
+const balance = new Balance(Infinity);
 
 class Park extends Cell {
   constructor() {
-    super("Reihenhäuser.png", "Park", 100);
-    this.color = "yellow";
+    super("img/park.png", "Park", 100);
     this.sidebar = new Sidebar();
     this.sidebar.add("button", {
-      content: "Upgraden für 250",
+      content: "Upgraden zu Spielplatz für 250",
       callback: () => {
-        console.log("ilhsdökhsödkj");
+        console.log("sdfbdsfbsdfbsdfbsdfbsdfbsdfbdsfbsdfb");
         if (!balance.buy(250)) return;
-        this.image.src = "pixil-frame-0 (1).png";
-      },
-    });
-    this.sidebar.add("button", {
-      content: "Upgraden für 500",
-      callback: () => {
-        if (!balance.buy(500)) return;
-        this.color = "blue";
+        this.image.src = "img/park2.png";
+        this.sidebar = new Sidebar();
+        this.sidebar.add("h3", { content: "Maximale Stufe erreicht" });
+        this.sidebar.open();
       },
     });
   }
@@ -39,22 +34,170 @@ class Park extends Cell {
 
 class Krankenhaus extends Cell {
   constructor() {
-    super("pixil-frame-0.png", "Krankenhaus", 100);
-    this.color = "green";
+    super("img/Krankenhaus1.png", "Krankenhaus", 100);
+    this.color = "yellow";
+    this.sidebar = new Sidebar();
+    this.sidebar.add("h3", { content: "Maximale Stufe erreicht" });
+  }
+  onclick() {
+    this.sidebar.open();
+  }
+}
+class Einkaufsladen extends Cell {
+  constructor() {
+    super("img/Einkaufsladen.png", "Einkaufsladen", 100);
+    this.color = "yellow";
+    this.sidebar = new Sidebar();
+    this.sidebar.add("h3", { content: "Maximale Stufe erreicht" });
+  }
+  onclick() {
+    this.sidebar.open();
+  }
+}
+
+class Fabrik extends Cell {
+  constructor() {
+    super("img/Fabrik.png", "Fabrik", 100);
+    this.color = "yellow";
+    this.sidebar = new Sidebar();
+    this.sidebar.add("h3", { content: "Maximale Stufe erreicht" });
+  }
+  onclick() {
+    this.sidebar.open();
+  }
+}
+class Kreuzung extends Cell {
+  constructor() {
+    super("img/KreuzungTemplate1.png", "Kreuzung", 0);
+    this.color = "yellow";
+    this.sidebar = new Sidebar();
+    this.sidebar.add("h3", { content: "Maximale Stufe erreicht" });
+  }
+  onclick() {
+    this.sidebar.open();
+  }
+}
+class Casino extends Cell {
+  constructor() {
+    super("img/Casino.png", "Casino", 100);
+    this.color = "yellow";
+    this.sidebar = new Sidebar();
+    this.sidebar.add("h3", { content: "Maximale Stufe erreicht" });
+  }
+  onclick() {
+    this.sidebar.open();
+  }
+}
+
+class Kindergarten extends Cell {
+  constructor() {
+    super("img/Kindergarten1.png", "Kindergarten", 100);
+    this.color = "yellow";
     this.sidebar = new Sidebar();
     this.sidebar.add("button", {
-      content: "Upgraden für 250",
+      content: "Upgraden zu Schule für 250",
       callback: () => {
-        console.log("ilhsdökhsödkj");
+        console.log("dfgsdfd");
         if (!balance.buy(250)) return;
-        this.color = "red";
+        this.image.src = "img/Schule1.png";
+        this.sidebar = new Sidebar();
+        this.sidebar.add("button", {
+          content: "Upgraden zu Uni für 250",
+          callback: () => {
+            console.log("sdfbdsfbsdfbsdfbsdfbsdfbsdfbdsfbsdfb");
+            if (!balance.buy(250)) return;
+            this.image.src = "img/Uni1.png";
+            this.sidebar = new Sidebar();
+            this.sidebar.add("h3", { content: "Maximale Stufe erreicht" });
+            this.sidebar.open();
+          },
+        });
+        this.sidebar.open();
       },
     });
+  }
+  onclick() {
+    this.sidebar.open();
+  }
+}
+class Häuser extends Cell {
+  constructor() {
+    super("img/Haueser1.png", "Häuser", 100);
+    this.color = "yellow";
+    this.sidebar = new Sidebar();
     this.sidebar.add("button", {
-      content: "Upgraden für 500",
+      content: "Upgraden zu Reihenhäuser für 250",
       callback: () => {
-        if (!balance.buy(500)) return;
-        this.color = "blue";
+        if (!balance.buy(250)) return;
+        this.image.src = "img/Reihenhäuser.png";
+        this.sidebar = new Sidebar();
+        this.sidebar.add("button", {
+          content: "Upgraden zu Plattenbau für 250",
+          callback: () => {
+            console.log("sdfbdsfbsdfbsdfbsdfbsdfbsdfbdsfbsdfb");
+            if (!balance.buy(250)) return;
+            this.image.src = "img/Plattenbau.png";
+
+            this.sidebar = new Sidebar();
+            this.sidebar.add("button", {
+              content: "Upgraden zu Plattenbau mit Solarplatten für 250",
+              callback: () => {
+                console.log("sdfbdsfbsdfbsdfbsdfbsdfbsdfbdsfbsdfb");
+                if (!balance.buy(250)) return;
+                this.image.src = "img/Plattenbau2.png";
+                this.sidebar = new Sidebar();
+                this.sidebar.add("button", {
+                  content: "Upgraden zu Wolkenkratzer für 250",
+                  callback: () => {
+                    console.log("sdfbdsfbsdfbsdfbsdfbsdfbsdfbdsfbsdfb");
+                    if (!balance.buy(250)) return;
+                    this.image.src = "img/Plattenbau2.png";
+                    this.sidebar = new Sidebar();
+                    this.sidebar.add("h3", {
+                      content: "Maximale Stufe erreicht",
+                    });
+                    this.sidebar.open();
+                  },
+                });
+                this.sidebar.open();
+              },
+            });
+            this.sidebar.open();
+          },
+        });
+        this.sidebar.open();
+      },
+    });
+  }
+  onclick() {
+    this.sidebar.open();
+  }
+}
+
+class UBahn extends Cell {
+  constructor() {
+    super("img/UBahn.png", "U-Bahn", 100);
+    this.color = "yellow";
+    this.sidebar = new Sidebar();
+    this.sidebar.add("h3", { content: "Maximale Stufe erreicht" });
+  }
+  onclick() {
+    this.sidebar.open();
+  }
+}
+class Einkaufsstraße extends Cell {
+  constructor() {
+    super("img/park.png", "Park", 100);
+    this.sidebar = new Sidebar();
+    this.sidebar.add("button", {
+      content: "Upgraden zu Spielplatz für 250",
+      callback: () => {
+        console.log("sdfbdsfbsdfbsdfbsdfbsdfbsdfbdsfbsdfb");
+        if (!balance.buy(250)) return;
+        this.image.src = "img/park2.png";
+        this.sidebar = new Sidebar();
+        this.sidebar.add("h3", { content: "Maximale Stufe erreicht" });
+        this.sidebar.open();
       },
     });
   }
@@ -70,4 +213,21 @@ htmlstuff.add("h2", "Deine Aufgabe:");
 
 htmlstuff.add("h3", "Baue eine nachhaltige Stadt");
 
-const grid = new Grid([Park, Krankenhaus], balance, 10, 10, 600, 600);
+const grid = new Grid(
+  [
+    Park,
+    Krankenhaus,
+    Häuser,
+    Kindergarten,
+    Fabrik,
+    Kreuzung,
+    Einkaufsladen,
+    Casino,
+    UBahn,
+  ],
+  balance,
+  10,
+  10,
+  600,
+  600
+);
